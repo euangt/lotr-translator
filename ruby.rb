10dba3abbcfa386abe52f713eec8cdf583bab2a9
euangt <euan.gillespie@outlook.com>
2021-01-28 17:04:01 +0000

    base sinatra incorporated

--- ruby.rb
+++ ruby.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'json'
 require 'open-uri'
+require 'sinatra'
 
 class APICalls
   attr_reader :accessor
@@ -43,31 +44,39 @@ class Translation
   end
 end
 
-def run_programme
-  result = ""
-  running = true
-  while running == true 
-    puts "Which language would you like to explore"
-    puts "1 - Sindarin"
-    puts "2 - Quenya"
-    puts "3 - Orcish"
-    puts "4 - Exit programme"
-    choice = gets.chomp
+# def run_programme
+#   result = ""
+#   running = true
+#   while running == true 
+#     puts "Which language would you like to explore"
+#     puts "1 - Sindarin"
+#     puts "2 - Quenya"
+#     puts "3 - Orcish"
+#     puts "4 - Exit programme"
+#     choice = gets.chomp
 
-    puts "What would you like to translate?"
-    input = gets.chomp
-    case choice
-    when "1"
-      result = APICalls.new(input).sindarin_api
-    when "2"
-      result = APICalls.new(input).quenya_api
-    when "3"
-      result = APICalls.new(input).orcish_api
-    when "4"
-      running = false
-    end
-    return Translation.new(result).translation
-  end
+#     puts "What would you like to translate?"
+#     input = gets.chomp
+#     case choice
+#     when "1"
+#       result = APICalls.new(input).sindarin_api
+#     when "2"
+#       result = APICalls.new(input).quenya_api
+#     when "3"
+#       result = APICalls.new(input).orcish_api
+#     when "4"
+#       running = false
+#     end
+#     return Translation.new(result).translation
+#   end
+# end
+# run_programme
+
+input = "hello"
+result = APICalls.new(input).sindarin_api
+translation = Translation.new(result)
+
+get '/' do
+  "Your search was for #{input}"
+  "And this translates to #{translation}"
 end
-
-run_programme
